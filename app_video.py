e759143c317a36b4a6928c1b370fdbeeb6f6d0b5
Dennis Langer <dennis.langer@students.fhnw.ch>
2024-11-21 16:43:42 +0100

    commit

--- app_video.py
+++ app_video.py
@@ -339,7 +339,7 @@ def save_game_to_pgn(moves, starting_fen):
     return pgn_string
 
 def main():
-    st.title("Schachspiel Analyse aus Video")
+    st.title("Schachspiel Analyse aus Video 22")
 
     # Video hochladen
     uploaded_file = st.file_uploader("Lade ein Video des Schachspiels hoch", type=["mp4", "avi", "mov"])
